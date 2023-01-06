a81efc9f641576e0a5bd19e82a25354dbe7b3754
syedash01 <20u12735@anjuman.edu.in>
2023-01-06 09:38:04 -1200

    add-card

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -12,3 +12,24 @@ document.querySelectorAll(".menu-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+/*vanilla-tilt.js*/
+const tilt = document.querySelectorAll(".tilt");
+
+VanillaTilt.init(tilt, {
+  reverse: true,
+  max: 15,
+  speed: 400,
+  scale: 1.12,
+  glare: true,
+  reset: true,
+  perspective: 500,
+  transition: true,
+  "max-glare": 0.75,
+  "glare-prerender": false,
+  gyroscope: true,
+  gyroscopeMinAngleX: -45,
+  gyroscopeMaxAngleX: 45,
+  gyroscopeMinAngleY: -45,
+  gyroscopeMaxAngleY: 45,
+});
